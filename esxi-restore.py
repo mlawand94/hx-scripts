36bee08f5250b84b8354bda60003d5b16d92c22a
mlawand94 <mlawand94@gmail.com>
2018-10-16 00:15:13 -0700

    Fixing vmk0 being configured, fixed active standby to active active on vswitch-hx-vm-network, and more

--- esxi-restore.py
+++ esxi-restore.py
@@ -153,13 +153,13 @@ def get_network_info():
             network_info[vmk] = str(vmk_network_info1)
 
 
-            netmask = raw_input("What is the netmask? ")
+            netmask = raw_input("What is the netmask?")
             while not validateIP(netmask):
                 print(netmask + " is a invalid IP address. Please try again")
                 netmask = raw_input("What is the netmask? ")
             network_info[(vmk+' - Netmask')] = netmask
 
-            gw = raw_input("What is the gateway? ")
+            gw = raw_input("What is the gateway? Input 0.0.0.0 if no G/W")
             while not validateIP(gw):
                 print(gw + ' is not a valid IP address. Please try again')
                 gw = raw_input("What is the gateway? ")
@@ -180,7 +180,7 @@ def get_network_info():
                 nm = input()
             network_info[(vmk+' - Netmask')] = nm
             
-            print("What is the gateway?")
+            print("What is the gateway? Input 0.0.0.0 if no G/W")
             gw = input()
             while not validateIP(gw):
                 print(gw + ' is an invalid IP. Please try again.')
